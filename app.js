4f9c245c0256f9a2646d4a56b1500f27aaf2c871
Flavia Penim <flaviapenim@gmail.com>
2017-11-22 15:59:23 +0000

    Correctly implemented if/then/else.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var Ajv = require('ajv');
 var ajv = new Ajv({allErrors: true});
 const fs = require('fs');
 
+// Schema load
 var inputSchema = fs.readFileSync('schemas/faang-schema.json');
 var jsonSchema = JSON.parse(inputSchema);
 var validate = ajv.compile(jsonSchema);
@@ -13,6 +14,15 @@ test(jsonAnimal);
 var inputCellSpecimen = fs.readFileSync('objects/faang-cellSpecimen-sample.json');
 var jsonCellSpecimen = JSON.parse(inputCellSpecimen);
 test(jsonCellSpecimen);
+
+/*
+// Example test
+var schema = JSON.parse(fs.readFileSync('schemas/example-schema.json'));
+var validate = ajv.compile(schema);
+
+var inputExample = JSON.parse(fs.readFileSync('objects/example.json'));
+test(inputExample);
+*/
 
 function test(data) {
   var valid = validate(data);
