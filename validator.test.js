8f092c8c25526ce3ce2e139664adc906b3d9ec96
Flavia Penim <fpenim@ebi.ac.uk>
2017-12-15 22:11:49 +0000

    Added BioSamples schema and validation tests.

--- validator.test.js
+++ validator.test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const runValidation = require('./validator');
 
-it('Empty schema, it should pass', () => {
+it('Empty Schema, it should pass', () => {
   let res = runValidation({}, {});
   
   expect(res).toBeA('object');
@@ -33,6 +33,30 @@ it('FAANG Schema, animal sample it should pass', () => {
 
 it('FAANG Schema, cell specimen sample it should pass', () => {
   var inputSchema = fs.readFileSync('examples/schemas/faang-schema.json');
+  var jsonSchema = JSON.parse(inputSchema);
+  
+  var inputObj = fs.readFileSync('examples/objects/faang-cellSpecimen-sample.json');
+  var jsonObj = JSON.parse(inputObj);
+  
+  let res = runValidation(jsonSchema, jsonObj);
+  expect(res).toBeA('object');
+  expect(res.result).toBeA('string').toBe('Valid!');
+});
+
+it('BioSamples Schema, animal sample it should pass', () => {
+  var inputSchema = fs.readFileSync('examples/schemas/biosamples-schema.json');
+  var jsonSchema = JSON.parse(inputSchema);
+  
+  var inputObj = fs.readFileSync('examples/objects/faang-animal-sample.json');
+  var jsonObj = JSON.parse(inputObj);
+  
+  let res = runValidation(jsonSchema, jsonObj);
+  expect(res).toBeA('object');
+  expect(res.result).toBeA('string').toBe('Valid!');
+});
+
+it('BioSamples Schema, cell specimen sample it should pass', () => {
+  var inputSchema = fs.readFileSync('examples/schemas/biosamples-schema.json');
   var jsonSchema = JSON.parse(inputSchema);
   
   var inputObj = fs.readFileSync('examples/objects/faang-cellSpecimen-sample.json');
