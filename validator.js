3514c541a4ceee29c5065129ab20507bc6e2e005
Flávia Penim <fpenim@ebi.ac.uk>
2017-12-21 10:28:07 +0000

    Schema improvement
    Schema improvement based on feedback received on https://github.com/epoberezkin/ajv/issues/650

--- validator.js
+++ validator.js
@@ -1,6 +1,18 @@
 var Ajv = require('ajv');
 var ajv = new Ajv({allErrors: true});
 
+ajv.addKeyword('isChildTerm', {
+  async: true,
+  type: 'integer',
+  validate: checkIsChildTerm
+});
+
+function checkIsChildTerm(schema, data) {
+  // TODO
+  console.log(schema);
+  return true;
+}
+
 function runValidation(inputSchema, submittable) {
   var validate = ajv.compile(inputSchema);
   var valid = validate(submittable);
