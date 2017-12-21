f13b9e200fb192e82ca63ca12318a92b1138c4a4
Flávia Penim <fpenim@ebi.ac.uk>
2017-12-21 17:41:52 +0000

    Start implementing validator with custom keywords

--- custom-validator.js
+++ custom-validator.js
@@ -1,6 +1,18 @@
 var Ajv = require('ajv');
 var ajv = new Ajv({allErrors: true});
 
+ajv.addKeyword('isChildTerm', {
+  async: true,
+  type: 'integer',
+  validate: checkIsChildTerm
+});
+
+function checkIsChildTerm(schema, data) {
+  // TODO
+  console.log(schema);
+  return true;
+}
+
 function runValidation(inputSchema, submittable) {
   var validate = ajv.compile(inputSchema);
   var valid = validate(submittable);
@@ -10,5 +22,3 @@ function runValidation(inputSchema, submittable) {
     return { result: 'Invalid: ' + ajv.errorsText(validate.errors)};
   }
 }
-
-module.exports = runValidation;
