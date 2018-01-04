46ee969a76513483377cbe97ab87ad43910d9576
Flávia Penim <fpenim@ebi.ac.uk>
2018-01-04 16:47:31 +0000

    Implementing term lookup without recursive call
    Implementation based on https://github.com/FAANG/validate-metadata/blob/master/lib/Bio/Metadata/Validate/Support/OlsLookup.pm#L169

--- src/ischildtermof.js
+++ src/ischildtermof.js
@@ -1,15 +1,69 @@
 'use strict';
 
+var Ajv = require('ajv');
 var request = require('request');
 
 module.exports = function defFunc(ajv) {
   defFunc.definition = {
     async: true,
     type: 'array',
-    validate: checkIsChildTermOf,
+    validate: findChildTerms,
     errors: true
   };
 
+  function findChildTerms(schema, data) {
+    return new Promise( function(resolve, reject) {
+      const parentTerm = schema.parentTerm;
+      const ontology = schema.ontology;
+      const termsArray = data;
+      const olsSearchUrl = "https://www.ebi.ac.uk/ols/api/search?q=";
+
+      if (findChildTerms.errors === null) {
+        findChildTerms.errors = [];
+      }
+
+      let errorCount = 0;
+      for (var i = 0; i < termsArray.length; i++) {
+        const termUri = encodeURIComponent(termsArray[i]);
+        const url = olsSearchUrl + termUri
+        + "&exact=true&groupField=true&allChildrenOf=" + encodeURIComponent(parentTerm)
+        + "&ontology=" + ontology + "&queryFields=iri";
+
+        request(url, function(error, response, body) {
+          let jsonBody = JSON.parse(body);
+          if(jsonBody.response.numFound === 1) {
+            resolve(true);
+          } else if(jsonBody.response.numFound === 0) {
+            findChildTerms.errors.push({
+              keyword: 'isChildTermOf',
+              message: 'is not child term of ' + parentTerm,
+              params: {keyword: 'isChildTermOf'}
+            });
+            errorCount++;
+            if (errorCount === termsArray.length) {
+              reject(new Ajv.ValidationError(findChildTerms.errors));
+            }
+          } else {
+            findChildTerms.errors.push({
+              keyword: 'isChildTermOf',
+              message: 'Something went wrong while validating term, try again.',
+              params: {keyword: 'isChildTermOf'}
+            });
+            errorCount++;
+            if (errorCount === termsArray.length) {
+              reject(new Ajv.ValidationError(findChildTerms.errors));
+            }
+          }
+        });
+      }
+    });
+  }
+
+
+
+
+
+/*
   function checkIsChildTermOf(schema, data) {
     return new Promise(function(resolve, reject) {
       const parentTerm = schema.parentTerm;
@@ -28,14 +82,15 @@ module.exports = function defFunc(ajv) {
             errorCount++;
             errors.push('No child term of ' + parentTerm + ' found.')
             if (errorCount === termsArray.length) {
-              resolve(errors)
+              reject(new Ajv.ValidationError(errors));
             }
           }
         }).catch(function(){
           errorCount++;
+          console.log(errorCount);
           errors.push('Could not access OLS API to check parent term.');
           if (errorCount === termsArray.length) {
-            resolve(errors)
+            reject(new Ajv.ValidationError(errors));
           }
         });
       }
@@ -54,10 +109,10 @@ module.exports = function defFunc(ajv) {
     if (responseBody) {
       let jsonBody = JSON.parse(responseBody);
       if (jsonBody._embedded.terms[0].iri === parentTerm) {
-        resolve(true);
+        resolveParam(true);
       }
       if (jsonBody._embedded.terms[0].is_root) {
-        resolve(false);
+        resolveParam(false);
       }
     }
 
@@ -68,12 +123,14 @@ module.exports = function defFunc(ajv) {
         let parentUrl = jsonBody._embedded.terms[0]._links.parents.href;
         findParentTerm(parentUrl, parentTerm, body, promiseParam, resolveParam, rejectParam);
       } else {
-        reject(error);
+        //reject(error);
+        console.log('failled....');
+        rejectParam(false);
       }
     });
     return promiseParam;
   }
-
+*/
   ajv.addKeyword('isChildTermOf', defFunc.definition);
   return ajv;
 };
