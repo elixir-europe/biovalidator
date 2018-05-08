f0f054f10d45b2432cb6f0393363722573c8275e
Flávia Penim <fpenim@ebi.ac.uk>
2018-05-08 14:09:20 +0100

    Enable CORS.

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(function(err, req, res, next) {
   if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
     res.status(400).send({"error": "Malformed JSON please check your request body."});
